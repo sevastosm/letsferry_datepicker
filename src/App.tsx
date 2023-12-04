b6a514f583466b0eeaf39024b254c18c2d6afa0e
CodeSandbox <support@codesandbox.io>
2023-12-04 20:36:19 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import { Calendar } from "primereact/calendar";
 import { Nullable } from "primereact/ts-helpers";
 import { InputText } from "primereact/inputtext";
-import moment from "moment";
 import { formatDate } from "./helpers";
 
 export default function BasicDemo() {
-  const [departureDate, setDepartureDate] = useState<Nullable<Date>>(null);
-  const [returnDate, setReturnDate] = useState<Nullable<Date>>(null);
+  const [departureDate, setDepartureDate] = useState<string | undefined>();
+  const [returnDate, setReturnDate] = useState<string | undefined>();
   const [date, setDate] = useState<Nullable<Date>>(null);
 
   console.log("selectedDate", date);
@@ -19,10 +18,20 @@ export default function BasicDemo() {
   const disabledDateList = ["12/09/23", "12/15/23", "12/22/23", "12/25/23"];
   const disabledDates = disabledDateList.map((date) => new Date(date));
 
-  const handleDateChage = (e) => {
+  const handleDateChage = (e: any) => {
     console.log(e);
+    setDate(e.value);
     setDepartureDate(formatDate(e.value[0]));
     setReturnDate(formatDate(e.value[1]));
+  };
+
+  const dateTemplate = (date: any) => {
+    console.log("date template", date);
+    if (date.today) {
+      return <strong className="text-blue-200 ">{date.day}</strong>;
+    }
+
+    return date.day;
   };
 
   return (
@@ -30,26 +39,26 @@ export default function BasicDemo() {
       <div className="card flex justify-content-center">
         <InputText
           value={departureDate}
-          //   onChange={(e) => setValue(e.target.value)}
+          onChange={(e) => setDepartureDate(e.target.value)}
           type="search"
         />
 
         <InputText
           value={returnDate}
-          //   onChange={(e) => setValue(e.target.value)}
+          onChange={(e) => setReturnDate(e.target.value)}
           type="search"
         />
       </div>
 
       <Calendar
         inline
-        type="search"
         value={date}
         onChange={handleDateChage}
         selectionMode="range"
         disabledDates={disabledDates}
         minDate={today}
         numberOfMonths={2}
+        dateTemplate={dateTemplate}
       />
     </div>
   );
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,7 @@
 import moment from "moment";
 
-export const formatDate = (date: Date, dateFormat = "M/D/Y") =>
-  moment(date).format(dateFormat);
+export const formatDate = (date: Date, dateFormat = "M/D/Y") => {
+  const d = moment(date).format(dateFormat);
+  console.log("formatDate", d);
+  return d === "Invalid date" ? "" : d;
+};
